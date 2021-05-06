13e4f5e8817be90364021515c258fde40bf199ac
MaxBuhe01 <mb@shopmodule.com>
2021-05-06 20:12:53 +0200

    add basic files

---
+++ src/Module/base.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Ant\Kallisto\base;
+
+
+class base
+{
+  public function __construct()
+  {
+    $this->run_Kallisto();
+  }
+
+  protected function run_Kallisto() : bool
+  {
+    return true;
+  }
+
+}
