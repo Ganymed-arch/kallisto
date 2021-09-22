a7f4013df4e5f124e90f4d7dad20060c65261c65
MaxBuhe01 <mb@shopmodule.com>
2021-09-22 19:59:39 +0200

    call runController instead of Model

--- src/index.php
+++ src/index.php
@@ -1,6 +1,9 @@
 <?php
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once "/www/htdocs/w01a761c/4chan/vendor/autoload.php";
+
+new \Kallisto\Controller\runController();
