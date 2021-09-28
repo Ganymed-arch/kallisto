c9bf5e63df8327d6c284627ac461f1a71a6d4c3b
MaxBuhe01 <mb@shopmodule.com>
2021-09-28 19:12:50 +0200

    #0017: short syntax inlcuding

--- src/index.php
+++ src/index.php
@@ -1,9 +1,11 @@
 <?php
 
+use Kallisto\Controller\runController;
+use Kallisto\Model\defineJsonContent;
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once "/www/htdocs/w01a761c/4chan/vendor/autoload.php";
 
-$oRun = new \Kallisto\Controller\runController("https://a.4cdn.org/boards.json");
+$oRun = new runController("https://a.4cdn.org/boards.json");
